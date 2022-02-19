eb7da638dc67b540243c1626b5f6edef3bc18433
ekjongh <ekjongh@gmail.com>
2022-02-19 22:04:21 +0900

    2.19 속도저하 및 5G->LTE 전환시 이벤트 구현

--- monitor/admin.py
+++ monitor/admin.py
@@ -1,5 +1,12 @@
 from django.contrib import admin
 from .models import Phone
+# from .forms import PhoneAdminForm
 
 # Register your models here.
-admin.site.register(Phone)
+@admin.register(Phone)
+class PhoneAdmin(admin.ModelAdmin):
+    list_display = ['phone_type', 'phone_no', 'networkId', 'avg_downloadBandwidth', \
+        'avg_uploadBandwidth', 'status', 'total_count', 'active']
+    list_display_links = ['phone_no']
+    search_fields = ('phone_no', )
+    list_filter = ['active',]
