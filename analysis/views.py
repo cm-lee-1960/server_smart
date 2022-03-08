eb7af83a3f91ec2751538d4e080e482e3a5816b4
unknown <zero0312@naver.com>
2022-03-08 17:11:27 +0900

    3.8 웹페이지 모달생성 및 현황 구체화

--- analysis/views.py
+++ analysis/views.py
@@ -6,10 +6,10 @@ import datetime
 
 # Create your views here.
 def make_report(request):
-    regi5G = Register.objects.get(category = "5G")
-    regiLTE = Register.objects.get(category = "LTE")
-    regiWiFi = Register.objects.get(category = "WiFi")
-    regiweak = Register.objects.get(category = "품질취약지역")
+    regi5G = Register.objects.filter(category = "5G")
+    regiLTE = Register.objects.filter(category = "LTE")
+    regiWiFi = Register.objects.filter(category = "WiFi")
+    regiweak = Register.objects.filter(category = "품질취약지역")
     firstdate = TodayRegister.objects.last()
     lastdate = TodayRegister.objects.first()
     
@@ -59,6 +59,31 @@ def make_report(request):
     hjd5gseoul = TodayRegister.objects.filter(category = "5G", dongdaco = "행정동", jiyok = "서울").count()
     hjd5gincheon = TodayRegister.objects.filter(category = "5G", dongdaco = "행정동", jiyok = "인천").count()
     hjd5gulsan = TodayRegister.objects.filter(category = "5G", dongdaco = "행정동", jiyok = "울산").count()
+    hjd5gdaegu = TodayRegister.objects.filter(category = "5G", dongdaco = "행정동", jiyok = "대구").count()
+    hjd5ggwangju = TodayRegister.objects.filter(category = "5G", dongdaco = "행정동", jiyok = "광주").count()
+    hjd5gdaejun = TodayRegister.objects.filter(category = "5G", dongdaco = "행정동", jiyok = "대전").count()
+    hjd5ggyunggi = TodayRegister.objects.filter(category = "5G", dongdaco = "행정동", jiyok = "경기").count()
+    hjd5ggyungbuk = TodayRegister.objects.filter(category = "5G", dongdaco = "행정동", jiyok = "경북").count()
+    hjd5gjunnam = TodayRegister.objects.filter(category = "5G", dongdaco = "행정동", jiyok = "전남").count()
+    hjd5gjunbuk = TodayRegister.objects.filter(category = "5G", dongdaco = "행정동", jiyok = "전북").count()
+    hjd5gchungnam = TodayRegister.objects.filter(category = "5G", dongdaco = "행정동", jiyok = "충남").count()
+    hjd5gchungbuk = TodayRegister.objects.filter(category = "5G", dongdaco = "행정동", jiyok = "충북").count()
+    hjd5gsejong = TodayRegister.objects.filter(category = "5G", dongdaco = "행정동", jiyok = "세종").count()
+
+    dagyo5gseoul = TodayRegister.objects.filter(category = "5G", dongdaco = "다중이용시설/교통인프라", jiyok = "서울").count()
+    dagyo5gincheon = TodayRegister.objects.filter(category = "5G", dongdaco = "다중이용시설/교통인프라", jiyok = "인천").count()
+    dagyo5gulsan = TodayRegister.objects.filter(category = "5G", dongdaco = "다중이용시설/교통인프라", jiyok = "울산").count()
+    dagyo5gdaegu = TodayRegister.objects.filter(category = "5G", dongdaco = "다중이용시설/교통인프라", jiyok = "대구").count()
+    dagyo5ggwangju = TodayRegister.objects.filter(category = "5G", dongdaco = "다중이용시설/교통인프라", jiyok = "광주").count()
+    dagyo5gdaejun = TodayRegister.objects.filter(category = "5G", dongdaco = "다중이용시설/교통인프라", jiyok = "대전").count()
+    dagyo5ggyunggi = TodayRegister.objects.filter(category = "5G", dongdaco = "다중이용시설/교통인프라", jiyok = "경기").count()
+    dagyo5ggyungbuk = TodayRegister.objects.filter(category = "5G", dongdaco = "다중이용시설/교통인프라", jiyok = "경북").count()
+    dagyo5gjunnam = TodayRegister.objects.filter(category = "5G", dongdaco = "다중이용시설/교통인프라", jiyok = "전남").count()
+    dagyo5gjunbuk = TodayRegister.objects.filter(category = "5G", dongdaco = "다중이용시설/교통인프라", jiyok = "전북").count()
+    dagyo5gchungnam = TodayRegister.objects.filter(category = "5G", dongdaco = "다중이용시설/교통인프라", jiyok = "충남").count()
+    dagyo5gchungbuk = TodayRegister.objects.filter(category = "5G", dongdaco = "다중이용시설/교통인프라", jiyok = "충북").count()
+    dagyo5gsejong = TodayRegister.objects.filter(category = "5G", dongdaco = "다중이용시설/교통인프라", jiyok = "세종").count()
+    dagyo5gsudo = TodayRegister.objects.filter(category = "5G", dongdaco = "다중이용시설/교통인프라", jiyok = "수도권").count()
 
 
     context = {'regi5G':regi5G,'regiLTE':regiLTE,'regiWiFi':regiWiFi,'regiweak':regiweak
@@ -69,6 +94,9 @@ def make_report(request):
     'sregiWiFi2':sregiWiFi2,'sregiWiFi':sregiWiFi,'tregiWiFi1':tregiWiFi1,'tregiWiFi2':tregiWiFi2,'tregiWiFi':tregiWiFi,'sregiweak1':sregiweak1,'sregiweak2':sregiweak2,
     'sregiweak3':sregiweak3,'sregiweak4':sregiweak4,'sregiweak':sregiweak,'tregiweak1':tregiweak1,'tregiweak2':tregiweak2,'tregiweak3':tregiweak3,'tregiweak4':tregiweak4,
     'tregiweak':tregiweak,'hjd5gseoul':hjd5gseoul, 'hjd5gincheon':hjd5gincheon, 'hjd5gulsan':hjd5gulsan,
+    "hjd5gseoul":hjd5gseoul,"hjd5gincheon":hjd5gincheon,"hjd5gulsan":hjd5gulsan,"hjd5gdaegu":hjd5gdaegu,"hjd5ggwangju":hjd5ggwangju,"hjd5gdaejun":hjd5gdaejun,"hjd5ggyunggi":hjd5ggyunggi,"hjd5ggyungbuk":hjd5ggyungbuk,"hjd5gjunnam":hjd5gjunnam,"hjd5gjunbuk":hjd5gjunbuk,"hjd5gchungnam":hjd5gchungnam,"hjd5gchungbuk":hjd5gchungbuk,"hjd5gsejong":hjd5gsejong,
+    "dagyo5gseoul":dagyo5gseoul,"dagyo5gincheon":dagyo5gincheon,"dagyo5gulsan":dagyo5gulsan,"dagyo5gdaegu":dagyo5gdaegu,"dagyo5ggwangju":dagyo5ggwangju,"dagyo5gdaejun":dagyo5gdaejun,"dagyo5ggyunggi":dagyo5ggyunggi,"dagyo5ggyungbuk":dagyo5ggyungbuk,"dagyo5gjunnam":dagyo5gjunnam,"dagyo5gjunbuk":dagyo5gjunbuk,"dagyo5gchungnam":dagyo5gchungnam,"dagyo5gchungbuk":dagyo5gchungbuk,"dagyo5gsejong":dagyo5gsejong,"dagyo5gsudo":dagyo5gsudo,
+
     }
    
     return render(request, "analysis/daily_report.html", context)
@@ -155,29 +183,38 @@ def make_register(request):
     if(request.method== "POST") :
         register = Register() #빈 객체 생성
         register.category = request.POST['category']
-        register.dong = request.POST['dong']
-        register.dagyo = request.POST['dagyo']
-        register.coverage = request.POST['coverage']
-        register.bigct = request.POST['bigct']
-        register.smallct = request.POST['smallct']
-        register.inbuiling = request.POST['inbuiling']
-        register.nong = request.POST['nong']
-        register.theme = request.POST['theme']
-        register.sangyong = request.POST['sangyong']
-        register.gaebang = request.POST['gaebang']
-        register.mountain =request.POST['mountain']
-        register.ship = request.POST['ship']
-        register.yooin = request.POST['yooin']
-        register.searoad = request.POST['searoad']
-    
         if(register.category == "5G"):
-            register.total = int(register.dong)+int(register.dagyo)+int(register.coverage)
+            register.dong = request.POST['dong']
+            register.dagyo = request.POST['dagyo']
+            register.coverage5g = request.POST['coverage5g']
+            register.total = int(register.dong)+int(register.dagyo)+int(register.coverage5g)
         elif(register.category == "LTE"):
-            register.total =  int(register.bigct)+ int(register.smallct)+ int(register.nong)+ int(register.inbuiling)+ int(register.theme)+ int(register.coverage)
+            register.bigct = request.POST['bigct']
+            register.smallct = request.POST['smallct']
+            register.inbuiling = request.POST['inbuiling']
+            register.nong = request.POST['nong']
+            register.theme = request.POST['theme']
+            register.coveragelte = request.POST['coveragelte']
+            register.total =  int(register.bigct)+ int(register.smallct)+ int(register.nong)+ int(register.inbuiling)+ int(register.theme)+ int(register.coveragelte)
         elif(register.category == "WiFi"):
+            register.sangyong = request.POST['sangyong']
+            register.gaebang = request.POST['gaebang']
             register.total =  int(register.sangyong)+ int(register.gaebang)
         else:
+            register.mountain =request.POST['mountain']
+            register.ship = request.POST['ship']
+            register.yooin = request.POST['yooin']
+            register.searoad = request.POST['searoad']
             register.total =  int(register.mountain)+ int(register.ship)+ int(register.yooin)+ int(register.searoad)
+        
+        # if(register.category == "5G"):
+        #     register.total = int(register.dong)+int(register.dagyo)+int(register.coverage)
+        # elif(register.category == "LTE"):
+        #     register.total =  int(register.bigct)+ int(register.smallct)+ int(register.nong)+ int(register.inbuiling)+ int(register.theme)+ int(register.coverage)
+        # elif(register.category == "WiFi"):
+        #     register.total =  int(register.sangyong)+ int(register.gaebang)
+        # else:
+        #     register.total =  int(register.mountain)+ int(register.ship)+ int(register.yooin)+ int(register.searoad)
         
             
         Register.objects.filter(category = register.category).delete()
