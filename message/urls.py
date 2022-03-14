1d6e68039d20660cac52145bf7e6b04ce97b9a54
bumess <cpsycs@hanmail.net>
2022-03-14 09:50:33 +0900

    (3.14) 크로샷 전송 임시 페이지 추가(/message/xroshot)

--- message/urls.py
+++ message/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path("start/", views.set_cb),
-    path("stop/", views.stop_cb)
+    path("stop/", views.stop_cb),
+    path("xroshot_send/", views.xroshot_send),
+    path("xroshot/", views.xroshot_index),
 ]
