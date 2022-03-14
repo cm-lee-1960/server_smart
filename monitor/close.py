5eee252e250aae0f0d4b2e0399a6532d5bffca60
cm-lee-1960 <ckdals232@gmail.com>
2022-03-14 11:40:31 +0900

    지역 종료 함수 1차 완성

--- monitor/close.py
+++ monitor/close.py
@@ -1,5 +1,6 @@
 from django.conf import settings
 from .models import Phone, PhoneGroup, Message
+from django.conf import settings
 
 ###################################################################################################
 # 측정종료 및 측정마감 모듈
@@ -10,12 +11,18 @@ from .models import Phone, PhoneGroup, Message
 ###################################################################################################
 
 def measuring_end(phonegroup,measdate):
+
+    channelId = settings.CHANNEL_ID
+    ##tele 채널아이디
+    
     '''해당지역 측정을 종효하는 함수'''
     # 1) 단말그룹: 상태변경
     # 2) 측정단말: 상태변경
     # 3) 해당지역 측정종료 메시지 생성 (유형: 텔레그램(TELE))
     # * 별도누적 테이블(모델) 필요 없음 - 검증을 위해 필요한 경우 메시지 모델에 항목 추가
     ## phonegroup == 폰그룹아이디, measdate == 측정날짜
+    ## 메인페이지의 종료 에어리어의 폰그룹 버튼을 눌럿을때는 
+    ## Phonegroup의 id값을 가지고 있어야된다.
     try:
         pg_check = PhoneGroup.objects.get(id=phonegroup,measdate=measdate)  
         if pg_check.active: ## phonegroup active가 1이면
@@ -33,10 +40,64 @@ def measuring_end(phonegroup,measdate):
         ## 모델업데이트
     except Exception as e:     
         print("지역측정종료 폰그룹 존재안함:",str(e))
-        ## 메세지 생성
+    
+    
+    ## 메세지 생성
+    #1. monitor view에서 low행 하나씩 들어올때마다 make 메세지실행    
+    #2. msg.py 에서 make_message함수로 메세지 생성후 message 모델 만들어지고
+    #3. post_save 실행 monitor model의 send_message함수실행으로
+    #4. bot.send_message_bot 실행
         
-    pass
-
+    #Q send_message는 왜 monitor model에잇는가        
+    ##mdata == measurecalldata
+    
+    ## 모델 상관관계
+    ## Phone Group -> Phone
+    ##             -> Message
+    
+    avg_downloadBandwidth = 0
+    avg_uploadBandwidth = 0
+    t_dl_count = 0
+    t_ul_count = 0
+    phone = pg_check.phone_set.all()
+    if len(phone) > 1:
+        for i in range(len(phone)):
+            globals()['f_phone_{}'.format(i)]=phone[i]
+            
+            avg_downloadBandwidth += globals()['f_phone_{}'.format(i)].avg_downloadBandwidth * globals()['f_phone_{}'.format(i)].dl_count
+            t_dl_count += globals()['f_phone_{}'.format(i)].dl_count
+            avg_uploadBandwidth += globals()['f_phone_{}'.format(i)].avg_uploadBandwidth * globals()['f_phone_{}'.format(i)].ul_count
+            t_ul_count += globals()['f_phone_{}'.format(i)].dl_count
+                         
+        # f_phone_1 = phone[0] ## 폰그룹중 첫번째
+        # f_phone_2 = phone[1]
+    else:
+        f_phone_0 = phone[0] ## 폰그룹중 첫번째
+        
+    mdata = f_phone_0.measurecalldata_set.filter(phone_id=f_phone_0.id)
+    
+    ## 폰아이디로 measurecalldata 추출 
+    
+    avg_downloadBandwidth = avg_downloadBandwidth / t_dl_count
+    avg_uploadBandwidth = avg_uploadBandwidth / t_ul_count
+    
+    messageContent = f"종료메세지" ## 수정
+                            
+    Message.objects.create(
+        phone=phone[0],  ## 폰그룹에서 첫번째 단말기 정보 기입
+        measdate=str(mdata.meastime)[0:8],
+        sendType='TELE',
+        userInfo1=mdata.userInfo1,
+        currentCount=mdata.currentCount,
+        phone_no=mdata.phone_no,
+        downloadBandwidth=avg_downloadBandwidth,
+        uploadBandwidth=avg_uploadBandwidth,
+        messageType='SMS',
+        message=messageContent,
+        channelId=channelId,
+        sended=True
+    )
+    
 def measuing_day_close():
     '''당일측정을 마감하는 함수'''
     # 1) 단말그룹: 상태변경 - 혹시 남아 있는 상태(True)
