28c96c1da7c3d226064298e28c453e2d5ffd3f5c
ekjongh <ekjongh@gmail.com>
2022-03-15 10:17:01 +0900

    측정종료 및 측정마감 파일 버젼 충돌로 수정 업데이트

--- monitor/close.py
+++ monitor/close.py
@@ -1,10 +1,8 @@
-<<<<<<< HEAD
+from email import message
 from django.conf import settings
 from .models import Phone, PhoneGroup, Message
 from django.conf import settings
 
-=======
->>>>>>> 0f277551f46bb7d33cd5d5a5e6c3cfaf6273b726
 ###################################################################################################
 # 측정종료 및 측정마감 모듈
 # 1) 측정종료 - 해당지역에 대한 측정을 종료처리
@@ -13,21 +11,16 @@ from django.conf import settings
 # 2022-03-11 - ???????
 ###################################################################################################
 
-<<<<<<< HEAD
 def measuring_end(phonegroup,measdate):
 
     channelId = settings.CHANNEL_ID
     ##tele 채널아이디
     
-=======
-def measuring_end():
->>>>>>> 0f277551f46bb7d33cd5d5a5e6c3cfaf6273b726
     '''해당지역 측정을 종효하는 함수'''
     # 1) 단말그룹: 상태변경
     # 2) 측정단말: 상태변경
     # 3) 해당지역 측정종료 메시지 생성 (유형: 텔레그램(TELE))
     # * 별도누적 테이블(모델) 필요 없음 - 검증을 위해 필요한 경우 메시지 모델에 항목 추가
-<<<<<<< HEAD
     ## phonegroup == 폰그룹아이디, measdate == 측정날짜
     ## 메인페이지의 종료 에어리어의 폰그룹 버튼을 눌럿을때는 
     ## Phonegroup의 id값을 가지고 있어야된다.
@@ -39,11 +32,13 @@ def measuring_end():
             ## active 0은 stop 모드
             p_check = pg_check.phone_set.all()
             p_check.update(active=0)
+            pg_check.save()
             ## phonegroup 하위 폰 active 일괄 업데이트
         else:
             pg_check.active = 1
             p_check = pg_check.phone_set.all()
             p_check.update(active=1)
+            pg_check.save()
         
         ## 모델업데이트
     except Exception as e:     
@@ -62,35 +57,44 @@ def measuring_end():
     ## 모델 상관관계
     ## Phone Group -> Phone
     ##             -> Message
+    ## phone그룹 단말기 정보
+    phone = pg_check.phone_set.all()
     
-    avg_downloadBandwidth = 0
+    ## 데이터 초기화
+    avg_downloadBandwidth = 0  
     avg_uploadBandwidth = 0
     t_dl_count = 0
     t_ul_count = 0
-    phone = pg_check.phone_set.all()
+    
+    f_phone_0 = phone[0] ##첫번째 단말기 정보는 기입
+    avg_downloadBandwidth = f_phone_0.avg_downloadBandwidth * f_phone_0.dl_count
+    t_dl_count = f_phone_0.dl_count
+    avg_uploadBandwidth = f_phone_0.avg_uploadBandwidth * f_phone_0.ul_count
+    t_ul_count = f_phone_0.ul_count
+
+    
     if len(phone) > 1:
-        for i in range(len(phone)):
-            globals()['f_phone_{}'.format(i)]=phone[i]
-            
+        for i in range(1, len(phone)):
+            globals()['f_phone_{}'.format(i)]=phone[i] ## 폰 여러개일 경우대비 변수 생성 변수 초기화 수정필요
+        
             avg_downloadBandwidth += globals()['f_phone_{}'.format(i)].avg_downloadBandwidth * globals()['f_phone_{}'.format(i)].dl_count
             t_dl_count += globals()['f_phone_{}'.format(i)].dl_count
             avg_uploadBandwidth += globals()['f_phone_{}'.format(i)].avg_uploadBandwidth * globals()['f_phone_{}'.format(i)].ul_count
-            t_ul_count += globals()['f_phone_{}'.format(i)].dl_count
+            t_ul_count += globals()['f_phone_{}'.format(i)].ul_count
                          
-        # f_phone_1 = phone[0] ## 폰그룹중 첫번째
-        # f_phone_2 = phone[1]
-    else:
-        f_phone_0 = phone[0] ## 폰그룹중 첫번째
+    # else:
+    #     f_phone_0 = phone[0] ## 폰그룹중 첫번째
         
-    mdata = f_phone_0.measurecalldata_set.filter(phone_id=f_phone_0.id)
+    mdata = f_phone_0.measurecalldata_set.filter(phone_id=f_phone_0.id).order_by('-meastime')[0] 
+    ##측정시간 기준으로 내림차순정렬의 첫번째 calldata를가져온다 즉 가장 최근측정 콜데이터
     
     ## 폰아이디로 measurecalldata 추출 
     
     avg_downloadBandwidth = avg_downloadBandwidth / t_dl_count
     avg_uploadBandwidth = avg_uploadBandwidth / t_ul_count
     
-    messageContent = f"종료메세지" ## 수정
-                            
+    messageContent = f"종료메세지== dl:" + str(avg_downloadBandwidth) +"ul:" +str(avg_uploadBandwidth)   ## 수정
+                
     Message.objects.create(
         phone=phone[0],  ## 폰그룹에서 첫번째 단말기 정보 기입
         measdate=str(mdata.meastime)[0:8],
@@ -106,10 +110,6 @@ def measuring_end():
         sended=True
     )
     
-=======
-    pass
-
->>>>>>> 0f277551f46bb7d33cd5d5a5e6c3cfaf6273b726
 def measuing_day_close():
     '''당일측정을 마감하는 함수'''
     # 1) 단말그룹: 상태변경 - 혹시 남아 있는 상태(True)
