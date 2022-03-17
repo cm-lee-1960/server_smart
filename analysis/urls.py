432899d024353bf00f3a9c180c4800474d3acfed
ekjongh <ekjongh@gmail.com>
2022-03-18 08:46:33 +0900

    루트패스에 로그인 페이지가 보여지도록 설정

--- analysis/urls.py
+++ analysis/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 app_name = 'analysis'
 urlpatterns = [
+    path('dashboard/',views.dashboard, name='dashboard'),
     path("report/", views.make_report, name='report'),
     path("register/", views.make_register),
     path('new/', views.new, name='new'),
@@ -10,6 +11,5 @@ urlpatterns = [
     path('make_todayregister/',views.make_todayregister,name='make_todayregister'),
     path('delete_register/',views.delete_register,name='delete_register'),
     path('delete_todayregister/',views.delete_todayregister,name='delete_todayregister'),
-    path('dashboard/',views.dashboard, name='dashboard'),
     path('make_report/',views.make_report, name='make_report'),
 ]
