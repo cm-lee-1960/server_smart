bba23c6e689f513bc9094469f6b94ca956a67585
ekjongh <ekjongh@gmail.com>
2022-04-13 15:56:42 +0900

    2주전 로그내역 삭제 모듈 작성 및 스케쥴러 등록

--- api/views.py
+++ api/views.py
@@ -223,34 +223,33 @@ def delete_message(request, message_id):
 @api_view(['POST'])
 def send_message(request):
     data = request.data
+    print("####### data:", data)
     result = {}
     try:
+        print("####")
         if 'sendType' in data.keys():
             sendType = data['sendType'] # 전송유형(sendType)
             receiver_list = data['receiver_list'] # 수신자 리스트
             message = data['message'] # 메시지 내용
             channelId = data['channelId'] # 채널ID
             message_id = data['id'] # 메시지ID
-            message_queryset = Message.objects.get(id=message_id)
-
+            message = Message.objects.get(id=message_id)
             # 1) 문자 메시지를 전송한다.
             if sendType == 'XMCS' or sendType == 'ALL':
                 from message.xmcs_msg import send_sms_queryset
                 receiver_list = receiver_list.replace(' ','').replace('\n','').split(',')
-                result_sms = send_sms_queryset(message_queryset, receiver_list)
+                result_sms = send_sms_queryset(message, receiver_list)
                 result = {'result': result_sms}
             # 2) 텔레그램 메시지를 재전송 한다.
             elif sendType == 'TELE':
                 bot = TelegramBot()
-                result = bot.send_message_bot(channelId, message)
-                if message_queryset.exists():
-                    message = message_queryset[0]
-                    message.telemessageId = result['message_id'] # 텔레그램 메시지ID
-                    message.sendTime = datetime.now() # 전송시간(보낸시간)
-                    message.isDel = False # 메시지 회수여부
+                result = bot.send_message_bot(channelId, message.message)
+                message.telemessageId = result['message_id'] # 텔레그램 메시지ID
+                message.sendTime = datetime.now() # 전송시간(보낸시간)
+                message.isDel = False # 메시지 회수여부
 
-                    # 메시지를 저장한다.
-                    message.save()
+                # 메시지를 저장한다.
+                message.save()
 
     except Exception as e:
         # 오류 코드 및 내용을 반환한다.
