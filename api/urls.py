8450c88d8731af777d10ea391ed507d6eb190ad1
ekjongh <ekjongh@gmail.com>
2022-05-02 09:51:13 +0900

    대시보드 화면 - REST API 주석 추가

--- api/urls.py
+++ api/urls.py
@@ -3,15 +3,35 @@ from . import views
 
 ########################################################################################################################
 # REST API 기능 제공
+#
+#  데시보드 화면(dashboard_form.html)
+#  ┌-----------------------------------------┐
+#  | ┌------------┐ ┌------------┐           |         시점: created, time interfal
+#  | | 오늘의측정 | | 센터별현황 |<----------╂----┰--- phonegroup/list/<str:measdate>/
+#  | |            | |            |           |    |    시점: 버튼 클릭시
+#  | └------------┘ └------------┘  종료/마감|<---|--- end/<int:phonegroup_id>/   <-- API 모듈로 랩핑할지 고민
+#  | ┌-------------------------------------┐ |    |    day_close/<str:measdate>/
+#  | | 단말그룹 현황                       |<╂----┘
+#  | |                                     | |
+#  | └-------------------------------------┘ |
+#  | ┌-----------------┐ ┌-----------------┐ |         시점: 단말그룹 선택(클릭)시
+#  | |이벤트/문자/텔레 | | 메시지상세내용  |<╂-------- message/list/<int:phonegroup_id>/
+#  | |                 | |                 | |         시점: 버튼 클릭시
+#  | |                 | |     전송/회수<--╂-╂-------- message/send/
+#  | └-----------------┘ └-----------------┘ |         message/delete/<int:message_id>/
+#  └-----------------------------------------┘
+# ----------------------------------------------------------------------------------------------------------------------
+# 2022.05.02 - 주석 추가
+#
 ########################################################################################################################
 app_name = 'api'
 urlpatterns = [
     path("phonegroup/list/<str:measdate>/", views.phonegroup_list, name='phoneGroupList'), # 단말그룹 리스트
     path("phonegroup/update/centerANDmorphology/", views.centerANDmorphology_list, name='centerANDmorphologyList'),  # 센터 및 모폴로지 리스트
-    path("phonegroup/update/info", views.update_phonegroup_info, name="PhoneGroupUpdate"),  # 단말그룹 정보 변경
+    path("phonegroup/update/info/", views.update_phonegroup_info, name="PhoneGroupUpdate"),  # 단말그룹 정보 변경
     path("message/list/<int:phonegroup_id>/", views.message_list, name='messageList'),  # 메시지 리스트
-    path("message/delete/<int:message_id>", views.delete_message, name='messageDelete'), # 텔레그램 메시지 전송취소
+    path("message/delete/<int:message_id>/", views.delete_message, name='messageDelete'), # 텔레그램 메시지 전송취소
     path("message/send/", views.send_message, name='messageSend'), # 문자 메시지 전송
-    path("message/get_chatmembers/<str:centerName>", views.get_chatmembers, name='getChatMembers'),  # 채팅방 멤버 리스트
+    path("message/get_chatmembers/<str:centerName>/", views.get_chatmembers, name='getChatMembers'),  # 채팅방 멤버 리스트
     path('monitor/makemap/<int:phonegroup_id>/', views.make_map, name='makemap') # 측정데이터 지도맵
 ]
