0823df04053c05ccc1dd1bc562b46b2fe0abcb74
ekjongh <ekjongh@gmail.com>
2022-05-18 10:23:13 +0900

    측정단말 사전정보 - 위치정보(시/도, 군,구, 상세주소, 위도, 경도), 파워온/오프 항목 추가

--- management/models.py
+++ management/models.py
@@ -25,6 +25,7 @@ from operator import itemgetter
 # 2022.03.28 - 센터정보 모델에 센터영문명(centerEngName) 항목 추가
 # 2022.04.28 - 측정단말에 대한 사전 정보 추가
 # 2022.05.18 - 센터정보 전송번호를 대표번호로 변경
+#            - 측정단말 사전정보(PhoneInfo)에 위치정보(시/도, 군,구, 상세주소, 위도, 경도), 파워온/오프 항목 추가
 #
 ########################################################################################################################
 
@@ -282,6 +283,13 @@ class PhoneInfo(models.Model):
                                  choices=sorted(NETWORKID_CHOICES, key=itemgetter(0)), verbose_name="유형")  # 네트워크ID(5G, LTE, 3G, WiFi)
     measuringTeam = models.CharField(max_length=20, null=True, blank=True, \
                                      choices=sorted(MEASURINGTEAM_CHOICES, key=itemgetter(0)), verbose_name='측정조')
+    siDo = models.CharField(max_length=100, null=True, blank=True, verbose_name="시,도")  # 시도
+    guGun = models.CharField(max_length=100, null=True, blank=True, verbose_name="군,구")  # 구,군
+    addressDetail = models.CharField(max_length=100, null=True, blank=True, verbose_name="상세주소")  # 주소상세
+    latitude = models.FloatField(null=True, blank=True, verbose_name="위도")  # 위도
+    longitude = models.FloatField(null=True, blank=True, verbose_name="경도")  # 경도
+    power = models.BooleanField(default=False, verbose_name="파워온/오프")  # 파워온/오프
+
 
     @property
     def phone_no_str(self):
