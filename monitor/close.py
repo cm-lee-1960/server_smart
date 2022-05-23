2cc5193df0b4fca016caa26cb92e8b23c394d2a9
bumess <cpsycs@hanmail.net>
2022-05-23 18:19:35 +0900

    5.23 전송성공률 계산 변경(전송실패 건수만 카운트)

--- monitor/close.py
+++ monitor/close.py
@@ -629,10 +629,10 @@ def cal_success_rate(phoneGroup):
     ''' 전송성공률 계산 함수 (초데이터)
      . 파라미터: phoneGroup
      . 반환값 : 성공률 (float) '''
-    # 전송 성공률 계산 : 전체 콜수에서 "전송실패" + "속도저하" 이벤트 발생 건수 비율로 계산 (추후 정확한 계산식 대체 필요)
+    # 전송 성공률 계산 : 전체 콜수에서 "전송실패" 이벤트 발생 건수 비율로 계산 (추후 정확한 계산식 대체 필요)
     md = phoneGroup.measuringdayclose_set.all().last()
     success_rate = round((1 - (Message.objects.filter(phoneGroup=phoneGroup, messageType='EVENT').filter( \
-                            Q(message__contains='전송실패') | Q(message__contains='속도저하')).count() / (md.dl_count + md.ul_count)))*100,1)
+                            message__contains='전송실패').count() / (md.dl_count + md.ul_count)))*100,1)
     return success_rate
 
 ###### 접속시간, LTE CA비율, 평균 DL/UL속도 등은 계산식 확인 후 업데이트 필요
