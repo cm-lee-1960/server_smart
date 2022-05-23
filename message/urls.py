f2c5662a1655081d3cc23885bd43a108de686105
bumess <cpsycs@hanmail.net>
2022-05-23 10:08:31 +0900

    5.23 불필요 코드/html파일 삭제

--- message/urls.py
+++ message/urls.py
@@ -4,7 +4,5 @@ from . import views
 urlpatterns = [
     path("telegram/bot_start/", views.set_cb),
     path("telegram/bot_stop/", views.stop_cb),
-    path("msg_send/", views.msg_send),
-    path("sample/", views.msg_index),
     # path("telegram/", views.listen_webhook)  // 추후 작성 예정(서버 연결 후)
 ]
