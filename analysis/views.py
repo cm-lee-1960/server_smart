7f174fe64960ad4dd2e8e121e000fab00f26c30f
cm-lee-1960 <ckdals232@gmail.com>
2022-05-31 16:35:12 +0900

    로그 확인

--- analysis/views.py
+++ analysis/views.py
@@ -173,8 +173,13 @@ def get_startdata(request):
 # -------------------------------------------------------------------------------------------------
 # 홈페이지 페이지
 # -------------------------------------------------------------------------------------------------
+######
+import logging
+db_logger = logging.getLogger('db')
+######
 def dashboard(request):
     """홈(Home) 페이지 뷰"""
+    db_logger.error("phonegroup_list():" + request.user.username + " / " + request.META.get('REMOTE_ADDR'))
     if request.user.is_authenticated:
         
         return render(request, "analysis/dashboard_form.html")
